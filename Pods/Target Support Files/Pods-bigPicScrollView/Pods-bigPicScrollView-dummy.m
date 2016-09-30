df425ba19e1c84d7b425a8a7feb29207d22ed29e
Chen <334670641@qq.com>
2016-09-30 10:51:31 +0800

    上传

---
+++ Pods/Target Support Files/Pods-bigPicScrollView/Pods-bigPicScrollView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bigPicScrollView : NSObject
+@end
+@implementation PodsDummy_Pods_bigPicScrollView
+@end
--- bigPicScrollView/kysScrollPicView.h
+++ bigPicScrollView/kysScrollPicView.h
@@ -10,8 +10,11 @@
 #import "UIImageView+WebCache.h"
 typedef NS_ENUM(NSInteger, kysAlignment)
 {
+    //点在中间
     kysAlignmentCenter = 0,
+    //点在左
     kysAlignmentLeft,
+    //点在右
     kysAlignmentRight
 };
 @protocol kysScrollPicDelegate<NSObject>
@@ -22,29 +25,29 @@ typedef NS_ENUM(NSInteger, kysAlignment)
 {
     //主视图
     UIScrollView *_mainScrollView;
-    //
+    //点
     UIPageControl *_pageController;
     //
     UILabel *_titleLabel;
-    //
+    //左视图图片
     UIImageView *_leftImageView;
-    //
+    //中视图图片
     UIImageView *_centerImageView;
-    //
+    //右视图图片
     UIImageView *_rightImageView;
-    //
+    //左视图名称
     UILabel *_leftTiTle;
-    //
+    //中视图名称
     UILabel *_centerTiTle;
-    //
+    //右视图名称
     UILabel *_rightTiTle;
-    //
+    //滚动时间
     NSTimer *_time;
     //数据
     NSArray *_titleList;
-    //
+    //图片数据
     NSArray *_imageList;
-    //
+    //当前页书
     NSInteger _currentIndex;
     //
     CGSize _baseSize;
@@ -54,13 +57,14 @@ typedef NS_ENUM(NSInteger, kysAlignment)
 @property (nonatomic,weak) id<kysScrollPicDelegate>delegate;
 //自动滚动时间
 @property (nonatomic) NSTimeInterval autoScrollTime;
+//样式
 - (void)setPageControllerAlignment:(kysAlignment)alignment;
-//
+//设置颜色
 - (void)setCurrentPageIndicatorColor:(UIColor*)current otherPageIndicator:(UIColor*)other;
-//
+//设置名称与图片地址
 - (void)setDatasWithTitleArray:(NSArray *)titleArray imageArray:(NSArray *)imageArray;
-//
+//暂停滚动
 - (void)pause;
-//
+//重新启动
 - (void)restart;
 @end
